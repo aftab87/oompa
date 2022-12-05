c97b97f723529767f708bed1c1ec61ddef0d4c90
juliatag <julieta.jabbot@gmail.com>
2022-12-05 18:06:10 -0500

    updated working menu

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,36 +1,40 @@
-
-import "./Stylesheets/header.css";
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-// import Button from 'react-bootstrap/Button';
-// import logo from "./images/testLogoImage.png";
-
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 // Need to include a dark and light mode
-
 function Header() {
-
-    return (
-        <Navbar collapseOnSelect expand="lg" bg="light" variant="bs-indigo" >
-            <div className="nav-con">
-                <Navbar href="#home" className="navLogo">Logo</Navbar>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#about">About</Nav.Link>
-                        <Nav.Link href="#login">
-                            <button className="loginButton">
-                                Login
-                            </button>
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </div>
-        </Navbar>
-    );
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <div href="#home" className="m-0 col-3">
+          <Form.Check type="switch" id="custom-switch" label="Dark Mode" />
+        </div>
+        <Navbar.Brand href="#home" className="m-0 col-6 text-center">
+          O ompa
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="m-0 col-3 text-end border-0" />
+        <Navbar.Collapse id="basic-navbar-nav justify-content-end" className="m-0 col-3 justify-content-end">
+          <Nav className="ms-auto text-center">
+            <Nav.Link className="d-flex align-items-center justify-content-center" href="/">
+              Home
+            </Nav.Link>
+            <Nav.Link className="d-flex align-items-center justify-content-center" href="/about">
+              About
+            </Nav.Link>
+            <Button variant="primary" className="m-2">
+              Login
+            </Button>
+            <Button variant="danger" className="m-2">
+              Logout
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Header;
