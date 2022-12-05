d7284e88cf246c4238a04609ab4bc8b67a4b795d
juliatag <julieta.jabbot@gmail.com>
2022-12-05 17:50:38 -0500

    fixed alignment, fully responsive and added buttons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -19,8 +19,18 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" className="m-0 col-3 text-end border-0" />
         <Navbar.Collapse id="basic-navbar-nav justify-content-end" className="m-0 col-3 justify-content-end">
           <Nav className="ms-auto text-center">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link className="d-flex align-items-center justify-content-center" href="/">
+              Home
+            </Nav.Link>
+            <Nav.Link className="d-flex align-items-center justify-content-center" href="/about">
+              About
+            </Nav.Link>
+            <Button variant="primary" className="m-2">
+              Login
+            </Button>
+            <Button variant="danger" className="m-2">
+              Logout
+            </Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
