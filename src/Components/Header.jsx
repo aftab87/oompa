9b39faac43c28fe4bbb2ccb73ad0c8136250f3d8
juliatag <julieta.jabbot@gmail.com>
2022-12-05 17:51:12 -0500

    cleanup

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -35,60 +35,6 @@ function Header() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-
-    // <Navbar bg="light" expand="lg">
-    //   {/* <Container fluid>
-    //     <Row> */}
-    //   <Col>
-    //     <Form.Check type="switch" id="custom-switch" label="Check this switch" />
-    //   </Col>
-    //   <Col className="text-center">
-    //     <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-    //   </Col>
-    //   <Col>
-    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //   </Col>
-    //   <Col>
-    //     <Navbar.Collapse id="basic-navbar-nav">
-    //       <Nav className="me-auto">
-    //         <Nav.Link href="/">Home</Nav.Link>
-    //         <Nav.Link href="#/about">About</Nav.Link>
-    //       </Nav>
-    //     </Navbar.Collapse>
-    //   </Col>
-    //   {/* </Row>
-    //   </Container> */}
-    // </Navbar>
-
-    // <Navbar bg="light" expand="xl" className="mb-3">
-    //     <Container fluid>
-    //         <Form.Check
-    //             type="switch"
-    //             id="darkSwitch"
-    //             label="Dark Mode"
-    //             className="flex-grow-1"
-    //         />
-    //         <Navbar.Brand href="/" className="flex-grow-1 text-center">Logo</Navbar.Brand>
-    //         <Navbar.Toggle aria-controls={`navBar`} />
-    //         <Navbar.Offcanvas
-    //             id={`navBar`}
-    //             aria-labelledby={`navBar-label`}
-    //             placement="end">
-    //             <Offcanvas.Header closeButton>
-    //                 <Offcanvas.Title id={`navBar-label`}>
-    //                     Welcome!
-    //                 </Offcanvas.Title>
-    //             </Offcanvas.Header>
-    //             <Offcanvas.Body>
-    //                 <Nav className="flex justify-content-end flex-grow-1">
-    //                     <Nav.Link href="/">Home</Nav.Link>
-    //                     <Nav.Link href="/about">About Us</Nav.Link>
-    //                     <Button variant="primary" >Sign up</Button>
-    //                 </Nav>
-    //             </Offcanvas.Body>
-    //         </Navbar.Offcanvas>
-    //     </Container>
-    // </Navbar>
   );
 }
 
