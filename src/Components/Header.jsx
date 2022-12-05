cd102f092aaad911dd998997f3c1280a76e37277
juliatag <julieta.jabbot@gmail.com>
2022-12-05 18:50:58 -0500

    fix page reload caused by bootstrap and react router combo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,31 +3,35 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link, NavLink } from "react-router-dom";
 
 // Need to include a dark and light mode
 function Header() {
   return (
     <Navbar bg="light" expand="lg">
+      {/* <NavLink className={({ isActive }) => (isActive ? "link-active" : "link-inactive")} to="/">
+        Home
+      </NavLink> */}
       <Container>
         <div href="#home" className="m-0 col-3">
           <Form.Check type="switch" id="custom-switch" label="Dark Mode" />
         </div>
-        <Navbar.Brand href="#home" className="m-0 col-6 text-center">
+        <Navbar.Brand as={Link} to="/" className="m-0 col-6 text-center">
           O ompa
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" className="m-0 col-3 text-end border-0" />
         <Navbar.Collapse id="basic-navbar-nav justify-content-end" className="m-0 col-3 justify-content-end">
           <Nav className="ms-auto text-center">
-            <Nav.Link className="d-flex align-items-center justify-content-center" href="/">
+            <Nav.Link as={Link} to="/" className="d-flex align-items-center justify-content-center">
               Home
             </Nav.Link>
-            <Nav.Link className="d-flex align-items-center justify-content-center" href="/about">
+            <Nav.Link as={Link} to="/about" className="d-flex align-items-center justify-content-center">
               About
             </Nav.Link>
-            <Button variant="primary" className="m-2">
+            <Button as={Link} variant="primary" className="m-2" to="/login">
               Login
             </Button>
-            <Button variant="danger" className="m-2">
+            <Button as={Link} variant="danger" className="m-2" to="/logout">
               Logout
             </Button>
           </Nav>
