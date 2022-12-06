4f97a4857532f3274df086b347e6b7a074cdf3d2
zac <zacclarke@gmail.com>
2022-12-06 18:37:10 -0500

    oogy

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,13 +1,11 @@
-import FormExample from "Examples/FormExample";
-import AddChore from "Examples/TestChoreBackEnd";
+
 import React from "react";
 
 function Dashboard(props) {
   return (
     <div>
       <h1> This is the Dashboard Page</h1>
-      <AddChore />
-      <FormExample />
+      
     </div>
   );
 }
