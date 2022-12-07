16b1a66a668556e06e42fbc8db6e89ef352b42e7
juliatag <julieta.jabbot@gmail.com>
2022-12-07 04:00:13 -0500

    Added Email Confirmation page

--- src/Pages/EmailConfirmation.jsx
+++ src/Pages/EmailConfirmation.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function EmailConfirmation(props) {
   return (
-    <div>
-      <h1> This is the Email Confirmation Page</h1>
-    </div>
+    <main className="email-confirmation text-center">
+      <section className="">
+        <div className="container my-5">
+          <div className="row justify-content-center align-items-center">
+            <div className="col">
+              <h2>Your Email Address has been confirmed</h2>
+              <p>You can now login with your email address and password</p>
+
+              <div className="text-center my-5">
+                <Button as={Link} to="/login" variant="primary">
+                  Login
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
 
