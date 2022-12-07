6f6c7a4446b0ac192456d691876c4f1886f377f9
juliatag <julieta.jabbot@gmail.com>
2022-12-07 16:43:42 -0500

    dashboard Main Area responsive

--- src/Components/Dashboard/Kids/KidMissions.jsx
+++ src/Components/Dashboard/Kids/KidMissions.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function KidMissions(props) {
-  return <div>kids missions section</div>;
+  return (
+    <section className="missions">
+      <div className="d-flex">
+        <h2>Your next Missions</h2>
+        <p>Complete missions to collect stars and exchange them for rewards</p>
+      </div>
+    </section>
+  );
 }
 
 export default KidMissions;
