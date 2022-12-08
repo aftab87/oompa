29eef27834fc8a76c929894d6fe9c165ab98c49a
juliatag <julieta.jabbot@gmail.com>
2022-12-08 01:37:54 -0500

    conflict fix

--- src/Pages/RewardsForm.jsx
+++ src/Pages/RewardsForm.jsx
@@ -1,23 +1,21 @@
-import RewardsCRUDForm from "Assets/Forms/RewardCRUDForm";
-import React, { useRef, useState } from 'react';
-
+import RewardsCRUDForm from "Components/Forms/RewardCRUDForm";
+import React, { useRef, useState } from "react";
 
 function RewardsForm() {
   // const reward_uidRef = useRef(value);
   // const kids_uidRef = useRef();
   // const date_redeemedRef = useRef();
   // const date_deliveredRef = useRef();
-  
+
   const reward_uidRef = "testing";
   const kids_uidRef = "Hello";
   const date_redeemedRef = "2022-12-09";
   const date_deliveredRef = "2022-11-09";
 
-  const [rewardHistoryId, setRewardHistoryId] = useState()
+  const [rewardHistoryId, setRewardHistoryId] = useState();
 
   const registeringRewardHistory = (event) => {
     const url = "http://localhost:3001/rewards/kids";
-
 
     event.preventDefault(); // prevent page reload
     // to fill in based on callPostBody
@@ -33,27 +31,23 @@ function RewardsForm() {
         "Content-type": "application/json; charset=UTF-8",
       },
     })
-      .then((data) => data.json()).then(data=>setRewardHistoryId(data._id))
+      .then((data) => data.json())
+      .then((data) => setRewardHistoryId(data._id));
   };
 
+  const modifyReward = () => {
+    const url = "http://localhost:3001/rewards/kids/" + rewardHistoryId;
 
-
-const modifyReward = ()=>{
-  
-  const url = "http://localhost:3001/rewards/kids/"+ rewardHistoryId;
-
-  fetch(url, {
-    method: "PUT",
-    body: JSON.stringify({
-      should_persist:true
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-  })
-    .then((data) => data.json())
-}
-
+    fetch(url, {
+      method: "PUT",
+      body: JSON.stringify({
+        should_persist: true,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then((data) => data.json());
+  };
 
   return (
     <>
