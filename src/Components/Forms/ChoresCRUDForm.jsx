c4bfc9ad35f76848f5ee12f230bb4e47dd3f9d5d
zac <zacclarke@gmail.com>
2022-12-08 06:26:59 -0500

    view CHORES and connect ADD CHORES FORM

--- src/Components/Forms/ChoresCRUDForm.jsx
+++ src/Components/Forms/ChoresCRUDForm.jsx
@@ -1,110 +1,108 @@
-import React, { useState, useRef, useEffect } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'Components/InputGroup';
-
+import React, { useState, useRef, useEffect, useContext } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "Components/InputGroup";
+import { userContext } from "App";
 
 function ChoresCRUDForm(props) {
-    const { missions } = props;
-    const [kids, setKids] = useState(null);
-    const [selectedKids, setSelectedKids] = useState([]);
-    const [validated, setValidated] = useState(false);
-    const [firstRun, setFirstRun] = useState(true);
+  const { missions } = props;
+  const [kids, setKids] = useState(null);
 
-    const titleRef = useRef();
-    const pointsRef = useRef();
-    const imageRef = useRef();
-    const startDateRef = useRef();
-    const endDateRef = useRef();
-    const repetitionRef = useRef();
+  const [selectedKids, setSelectedKids] = useState([]);
+  const [validated, setValidated] = useState(false);
+  const [firstRun, setFirstRun] = useState(true);
+  const [user, setUser] = useContext(userContext);
 
-    async function callGetAllKids() {
-        // callGetAllKids
-        await fetch("http://localhost:3001/kids", { method: "GET" })
-            .then((data) => data.json())
-            .then((json) => json)
-            .then((json) => setKids(json));
+  const titleRef = useRef();
+  const pointsRef = useRef();
+  const imageRef = useRef();
+  const startDateRef = useRef();
+  const endDateRef = useRef();
+  const repetitionRef = useRef();
+
+  async function callGetAllKids() {
+    // callGetAllKids
+    await fetch("http://localhost:3001/kids/" + user.id, { method: "GET" })
+      .then((data) => data.json())
+      .then((json) => json)
+      .then((json) => setKids(json));
+  }
+
+  useEffect(() => {
+    if (firstRun) {
+      setFirstRun(false);
+      callGetAllKids();
+    }
+    if (missions) {
+      console.log(missions);
+      titleRef.current.value = missions.title;
+      startDateRef.current.value = missions.startDate;
+      endDateRef.current.value = missions.endDate;
+      imageRef.current.value = missions.image;
+      pointsRef.current.value = missions.points;
+      repetitionRef.current.value = missions.repetition;
+    }
+  }, [firstRun, missions]);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
+      registeringReward(event);
     }
 
+    setValidated(true);
+  };
 
-    useEffect(() => {
-        if (firstRun) {
-            setFirstRun(false)
-            callGetAllKids()
-        }
-        if (missions) {
-            console.log(missions)
-            titleRef.current.value = missions.title
-            startDateRef.current.value = missions.startDate
-            endDateRef.current.value = missions.endDate
-            imageRef.current.value = missions.image
-            pointsRef.current.value = missions.points
-            repetitionRef.current.value = missions.repetition
-        }
-    }, [firstRun, missions])
+  const registeringReward = (event) => {
+    const url = missions ? "http://localhost:3001/chores/" + missions._id : "http://localhost:3001/chores/";
 
-    const handleSubmit = (event) => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-        } else {
-            registeringReward(event)
-        }
+    event.preventDefault(); // prevent page reload
+    // to fill in based on callPostBody
+    fetch(url, {
+      method: missions ? "PUT" : "POST",
+      body: JSON.stringify({
+        parent_uid: user.id,
+        title: titleRef.current.value,
+        start_date: startDateRef.current.value,
+        end_date: endDateRef.current.value,
+        kids: selectedKids,
+        image: imageRef.current.value,
+        points: pointsRef.current.value,
+        repetition: repetitionRef.current.value,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then((data) => data.json());
+  };
 
-        setValidated(true);
-    };
-
-
-
-    const registeringReward = (event) => {
-        const url = missions ? "http://localhost:3001/dashboard/rewards/" + missions._id : "http://localhost:3001/dashboard/missions/"
-
-        event.preventDefault(); // prevent page reload
-        // to fill in based on callPostBody
-        fetch(url, {
-            method: missions ? "PUT" : "POST",
-            body: JSON.stringify({
-                title: titleRef.current.value,
-                startDate: startDateRef.current.value,
-                endDate: endDateRef.current.value,
-                kids: selectedKids,
-                image: imageRef.current.value,
-                points: pointsRef.current.value,
-                repetition: repetitionRef.current.value,
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-            },
-        })
-            .then((data) => data.json())
-    };
-
-    const handleKidsSelection = (username, selected) => {
-
-        if (selected && !selectedKids.includes(username)) {
-            // console.log(`adding ${username} to selected`)
-            setSelectedKids(selectedKids => {
-                let temp = [...selectedKids, username]
-                return temp;
-            })
-        } else if (!selected && selectedKids.includes(username)) {
-            // console.log(`removing ${username} from selected`)
-            setSelectedKids(selectedKids => {
-                let temp = [...selectedKids]
-                return temp.filter(kid => kid !== username)
-            })
-        }
+  const handleKidsSelection = (username, selected) => {
+    if (selected && !selectedKids.includes(username)) {
+      // console.log(`adding ${username} to selected`)
+      setSelectedKids((selectedKids) => {
+        let temp = [...selectedKids, username];
+        return temp;
+      });
+    } else if (!selected && selectedKids.includes(username)) {
+      // console.log(`removing ${username} from selected`)
+      setSelectedKids((selectedKids) => {
+        let temp = [...selectedKids];
+        return temp.filter((kid) => kid !== username);
+      });
     }
+  };
 
-    return (
-        <div className="container">
-            <div className='form'>
-                <div className="text-center my-5">
-                    {props.title && <h1>{props.title}</h1>}
-                    {props.subtitle && <h3>{props.subtitle}</h3>}
-                </div>
-                {/* <Form noValidate validated={validated} onSubmit={handleSubmit} >
+  return (
+    <div className="container">
+      <div className="form">
+        <div className="text-center my-5">
+          {props.title && <h1>{props.title}</h1>}
+          {props.subtitle && <h3>{props.subtitle}</h3>}
+        </div>
+        {/* <Form noValidate validated={validated} onSubmit={handleSubmit} >
 
                     <InputGroup type="text" label="Title" placeholder="Name of mission..." required ref={titleRef} />
                     <InputGroup type="text" as="textarea" rows={4} label="Description" placeholder="Please describe reward..." required ref={descriptionRef} />
@@ -116,33 +114,25 @@ function ChoresCRUDForm(props) {
                     </div>
                 </Form>
             </div> */}
-                <Form noValidate validated={validated} onSubmit={handleSubmit} >
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <InputGroup type="text" label="Title of Mission(s)" placeholder="Name of missions..." required ref={titleRef} />
+          <InputGroup type="number" label="Points" placeholder="Please enter # points" required ref={pointsRef} />
+          {/* TODO:MAKE CUSTOM LABEL */}
+          <Form.Label>Kids</Form.Label>
+          <div className="d-flex gap-3">{kids && kids.map((kid) => <InputGroup type="checkbox" key={kid._id} label={kid.first_name} value={kid._id} onKidChange={handleKidsSelection} />)}</div>
+          {/* <InputGroup type="text" label="Select Child" placeholder="Child name..." required ref={childRef} /> */}
+          <InputGroup type="text" label="Image" placeholder="Insert image..." required ref={imageRef} />
+          <InputGroup type="date" label="Start Date" placeholder="Enter start date..." required ref={startDateRef} />
+          <InputGroup type="date" label="End Date" placeholder="Enter end date..." required ref={endDateRef} />
+          <InputGroup type="number" label="Repitions" placeholder="How many times..." required ref={repetitionRef} />
 
-                    <InputGroup type="text" label="Title of Mission(s)" placeholder="Name of missions..." required ref={titleRef} />
-                    <InputGroup type="number"  label="Points" placeholder="Please enter # points" required ref={pointsRef} />
-                    {/* TODO:MAKE CUSTOM LABEL */}
-                    <Form.Label>Kids</Form.Label>
-                    <div className="d-flex gap-3" >
-                        {kids &&
-                            kids.map((kid) => <InputGroup type="checkbox" key={kid._id} label={kid.first_name} value={kid.username} onKidChange={handleKidsSelection} />)
-                        }
-                    </div>
-                    {/* <InputGroup type="text" label="Select Child" placeholder="Child name..." required ref={childRef} /> */}
-                    <InputGroup type="text" label="Image" placeholder="Insert image..." required ref={imageRef} />
-                    <InputGroup type="date" label="Start Date" placeholder="Enter start date..." required ref={startDateRef} />
-                    <InputGroup type="date" label="End Date" placeholder="Enter end date..." required ref={endDateRef} />
-                    <InputGroup type="number" label="Repitions" placeholder="How many times..." required ref={repetitionRef} />
-
-                    <div className='text-center'>
-                        <Button type="submit" >Submit form</Button>
-                    </div>
-                </Form>
-            
-            </div>
-        </div>
-    );
+          <div className="text-center">
+            <Button type="submit">Submit form</Button>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
 export default ChoresCRUDForm;
-
-
