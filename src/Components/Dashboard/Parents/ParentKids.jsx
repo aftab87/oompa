e3abbc7ac0606bc2a2f05dcd11308a06e4b3b997
zac <zacclarke@gmail.com>
2022-12-09 10:03:58 -0500

    fixed kids view

--- src/Components/Dashboard/Parents/ParentKids.jsx
+++ src/Components/Dashboard/Parents/ParentKids.jsx
@@ -12,7 +12,7 @@ function KidOverview(props) {
   const [firstRun, setFirstRun] = useState(true);
 
   async function callGetAllKids() {
-    await fetch("http://localhost:3001/kidsavatar/" + user.id, { method: "GET" })
+    await fetch("http://localhost:3001/kids/" + user.id, { method: "GET" })
       .then((data) => data.json())
       .then((json) => json)
       .then((json) => setKids(json));
@@ -37,16 +37,14 @@ function KidOverview(props) {
           Add Kid
         </Button>
       </div>
-
       {/* tab buttons */}
-      {/* <div className="d-flex justify-content-center gap-3 p-4">
+      <div className="d-flex justify-content-center gap-3 p-4">
         <DashboardTabButton label={"Tommy"} section={"missions"} endpoint={"available"} />
         <DashboardTabButton label={"John"} section={"missions"} endpoint={"completed"} />
         <DashboardTabButton label={"Jane"} section={"missions"} endpoint={"approved"} />
       </div>
-      <div className="row g-3">{kids && kids.map((kids) => <KidsCard key={kids._id} id={kids._id} stars={kids.points} title={kids.username} img="" date="Monday" time="7:30 pm" kids={kids.first_name} state="available" description={kids.description} onDelete={deleteHandler} />)}</div> */}
-
-      {/* tab content */}
+      <div className="row g-3">{kids && kids.map((kids) => <KidsCard key={kids._id} id={kids._id} stars={kids.points} title={kids.username} img="" date="Monday" time="7:30 pm" kids={kids.first_name} state="available" description={kids.description} onDelete={deleteHandler} />)}</div>
+      {/* /* tab content */}
       <Outlet />
     </section>
   );
