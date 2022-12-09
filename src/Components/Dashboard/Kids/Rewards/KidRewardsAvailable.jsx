5801863728a2ba8cd024389d930238a5caed1b00
juliatag <julieta.jabbot@gmail.com>
2022-12-09 00:23:41 -0500

    Reward card progress bar done

--- src/Components/Dashboard/Kids/Rewards/KidRewardsAvailable.jsx
+++ src/Components/Dashboard/Kids/Rewards/KidRewardsAvailable.jsx
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { userContext } from "App";
+import RewardCard from "Components/Dashboard/RewardCard";
 
 function KidRewardsAvailable(props) {
+  async function callGetAllRewards() {
+    await fetch("http://localhost:3001/dashboard/rewards/" + user.parent_uid, { method: "GET" }) //keep until a method by kid id available
+      // await fetch("http://localhost:3001/dashboard/rewards/" + user.id, { method: "GET" })
+      .then((data) => data.json())
+      .then((json) => json)
+      .then((json) => setRewards(json));
+  }
+  const [rewards, setRewards] = useState(null);
+  const [user, setUser] = useContext(userContext);
+  const [firstRun, setFirstRun] = useState(true);
+  console.log(user);
+
+  useEffect(() => {
+    if (firstRun) {
+      setFirstRun(false);
+      callGetAllRewards();
+    }
+  }, [firstRun, rewards]);
   return (
-    <div className="d-flex gap-4 flex-wrap justify-content-center p-4">
-      <div className="btn-square bg-warning">card placeholder</div>
-      <div className="btn-square bg-warning">card placeholder</div>
-      <div className="btn-square bg-warning">card placeholder</div>
-      <div className="btn-square bg-warning">card placeholder</div>
-    </div>
+    <>
+      <Button variant="primary" as={NavLink} to="/dashboard/rewards/add">
+        Add New Reward
+      </Button>
+
+      <div className="row g-3">
+        {/*!!!!!!!!!!! Temp mapping multiple times to pass missing data as props. Once we know what we need, we can adjust the model */}
+        <div className="row g-3">{rewards && rewards.map((reward) => <RewardCard key={reward._id} reward={reward} date="" time="" state="available" />)}</div>
+      </div>
+
+      <div className="p-4 bg-white border m-4 rounded-4">
+        <h3>Test Edit Rewards</h3>
+        <p>Make changes and/or delete rewards.</p>
+        <Button variant="primary" as={NavLink} to="/dashboard/rewards/edit?reward=63912e8d5d9afcff48f748d3">
+          Edit Test Reward
+        </Button>
+      </div>
+    </>
   );
 }
 
