36805d6ea4131063b186f8434289aef1d4f28d40
zac <zacclarke@gmail.com>
2022-12-09 07:45:39 -0500

    removed text from under picture in kids overview

--- src/Components/Dashboard/Kids/KidOverview.jsx
+++ src/Components/Dashboard/Kids/KidOverview.jsx
@@ -10,7 +10,7 @@ function KidOverview(props) {
   return (
     <section className="overview">
       <div className="d-flex justify-content-between">
-        <SectionHeader title={"Welcome back " + user.first_name} text={"Complete missions to collect stars and exchange them for rewards"} />
+        <SectionHeader title={"Welcome back " + user.first_name + "!"} text={"Complete missions to collect stars and exchange them for rewards"} />
         <StarCounter numStars={16} />
       </div>
 
@@ -18,7 +18,7 @@ function KidOverview(props) {
       <div className="row">
         <div className="col-12 col-md-6 text-center">
         <img src={`/images/avatar${user.avatar}.svg`} alt="main avatar" width="250" className="img-fluid" />
-        <h2>You can select your avatar</h2>
+        <h2></h2>
         </div>
         <div className=" col-12 col-md-6 d-flex justify-content-start">
           <div className="row">
